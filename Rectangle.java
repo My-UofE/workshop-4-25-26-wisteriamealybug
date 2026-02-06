071af93f5da1aaacd8e7f4dc098632b31d772b79
catofsedum <memh201@exeter.ac.uk>
2026-02-06 11:43:13 +0000

    completed part 1

--- Rectangle.java
+++ Rectangle.java
@@ -1,37 +1,45 @@
 // A Rectangle class
 public class Rectangle {
-  
-  // 4 instance attributes
-  public double width;
-  public double height;
-  public double originX = 0.0;
-  public double originY = 0.0;
 
-  // 1 static attributes
-  public static final int NUMBER_OF_SIDES = 4;
+    // 4 instance attributes
+    public double width;
+    public double height;
+    public double originX = 0.0;
+    public double originY = 0.0;
 
-  // main constructor
-  public Rectangle(double w, double h, double oX, double oY) {
-    width = w;
-    height = h;
-    originX = oX;
-    originY = oY;
-  }
+    // 1 static attributes
+    public static final int NUMBER_OF_SIDES = 4;
 
-  // method: move the rectangle
-  public void move(double dx, double dy) {
-	   originX += dx;
-	   originY += dy;
-  }
+    // main constructor
+    public Rectangle(double width, double height, double originX, double originY) {
+        this.width = width;
+        this.height = height;
+        this.originX = originX;
+        this.originY = originY;
+    }
 
-  // method: compute the area of the rectangle
-  public double getArea() {
-    return width * height;
-  }
-  
-  // method: compute the area of the rectangle
-  public double getPerimeter() {
-    return 2 * (width + height);
-  }
-  
+    public Rectangle(double width, double height) {
+        this(width, height, 0, 0);
+    }
+
+    public Rectangle() {
+        this(1, 1);
+    }
+
+    // method: move the rectangle
+    public void move(double dx, double dy) {
+        originX += dx;
+        originY += dy;
+    }
+
+    // method: compute the area of the rectangle
+    public double getArea() {
+        return width * height;
+    }
+
+    // method: compute the area of the rectangle
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
 }
--- RectangleApp.java
+++ RectangleApp.java
@@ -1,42 +1,37 @@
 // An application to create and manipulate rectangles
 public class RectangleApp {
-	//To be excutable, need a main method
-	public static void main( String[] args ) {
-		System.out.println("Creating myRect1");
-		
-		Rectangle myRect1; //myRect is not instantiated
-		myRect1 = new Rectangle(20.0, 8.0,30,30); //instantiated
-		
-		//static field
-		System.out.println("Rectangle has " + Rectangle.NUMBER_OF_SIDES + " sides");
-		//instance fields
-		System.out.println("Width: "+myRect1.width+", Height: "+myRect1.height);
-		System.out.println("Origin is: "+myRect1.originX+","+myRect1.originY);
-		//calling methods
-		System.out.println("Area: "+myRect1.getArea());
-		
-		System.out.println("\nMoving myRect1");
-		myRect1.move(0,10);//the object's state is changed
-		System.out.println("Origin is: "+myRect1.originX+","+myRect1.originY);
+    // To be excutable, need a main method
+    public static void main(String[] args) {
+        System.out.println("Creating myRect1");
 
-		System.out.println("Changing width of myRect1");
-		myRect1.width = 16;//the object's state is changed
-		System.out.println("Width: "+myRect1.width+", Height: "+myRect1.height);
+        Rectangle myRect1; // myRect is not instantiated
+        myRect1 = new Rectangle(20.0, 8.0, 30, 30); // instantiated
 
-		/*
-		System.out.println("Creating myRect2");
-		Rectangle myRect2 = new Rectangle(20.0, 8.0);
-		System.out.println("Width: "+myRect2.width+", Height: "+myRect2.height);
-		System.out.println("Origin: "+myRect2.originX+","+myRect2.originY);
-		 */
+        // static field
+        System.out.println("Rectangle has " + Rectangle.NUMBER_OF_SIDES + " sides");
+        // instance fields
+        System.out.println("Width: " + myRect1.width + ", Height: " + myRect1.height);
+        System.out.println("Origin is: " + myRect1.originX + "," + myRect1.originY);
+        // calling methods
+        System.out.println("Area: " + myRect1.getArea());
 
+        System.out.println("\nMoving myRect1");
+        myRect1.move(0, 10);// the object's state is changed
+        System.out.println("Origin is: " + myRect1.originX + "," + myRect1.originY);
 
-		/*
-		System.out.println("Creating myRect3");
-		Rectangle myRect3 = new Rectangle(); 
-		System.out.println("Width: "+myRect3.width+", Height: "+myRect3.height);
-		System.out.println("Origin: "+myRect3.originX+","+myRect3.originY);
-		 */
+        System.out.println("Changing width of myRect1");
+        myRect1.width = 16;// the object's state is changed
+        System.out.println("Width: " + myRect1.width + ", Height: " + myRect1.height);
 
-	}
+        System.out.println("Creating myRect2");
+        Rectangle myRect2 = new Rectangle(20.0, 8.0);
+        System.out.println("Width: " + myRect2.width + ", Height: " + myRect2.height);
+        System.out.println("Origin: " + myRect2.originX + "," + myRect2.originY);
+
+        System.out.println("Creating myRect3");
+        Rectangle myRect3 = new Rectangle();
+        System.out.println("Width: " + myRect3.width + ", Height: " + myRect3.height);
+        System.out.println("Origin: " + myRect3.originX + "," + myRect3.originY);
+
+    }
 }
